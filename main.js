209c24e2f686e38df0a750ebc8c57e6eaf8df610
edysouza363@gmail.com <edysouza363@gmail.com>
2023-07-22 22:17:51 -0300

    atualização raquete e colisão

--- main.js
+++ main.js
@@ -15,38 +15,57 @@ let raqueteComprimento = 10;
 let raqueteAltura = 90
 
 function setup() {
-  createCanvas(600 , 400);
+  createCanvas(600, 400);
 }
 
 function draw() {
   background(0);
   mostraBolinha();
-  //movimentaBolinha();
+  movimentaBolinha();
   verificaColisaoBorda();
   mostraRaquete();
-  
+  movimentaMinhaRaquete();
+  verificaColisaoRaquete();
+
 }
 
-function mostraBolinha(){
+function mostraBolinha() {
   circle(xBolinha, yBolinha, diametro);
 }
 
-function movimentaBolinha(){
+function movimentaBolinha() {
   xBolinha += velocidadeXBolinha
   yBolinha += velocidadeYBolinha
 }
 
-function verificaColisaoBorda(){
-    if (xBolinha + raio > width ||
-     xBolinha - raio < 0){
+function verificaColisaoBorda() {
+  if (xBolinha + raio > width ||
+    xBolinha - raio < 0) {
     velocidadeXBolinha *= -1;
   }
-  if(yBolinha + raio > height ||
-     yBolinha - raio < 0){
+  if (yBolinha + raio > height ||
+    yBolinha - raio < 0) {
     velocidadeYBolinha *= -1;
   }
 }
 
-function mostraRaquete(){
+function mostraRaquete() {
   rect(xRaquete, yRaquete, raqueteComprimento, raqueteAltura);
+}
+
+function movimentaMinhaRaquete() {
+  if (keyIsDown(UP_ARROW)) {
+    yRaquete -= 10;
+  }
+  if (keyIsDown(DOWN_ARROW)) {
+    yRaquete += 10;
+  }
+}
+
+function verificaColisaoRaquete() {
+  if (xBolinha - raio < xRaquete + raqueteComprimento
+    && yBolinha - raio < yRaquete + raqueteAltura &&
+    yBolinha + raio > yRaquete) {
+    velocidadeXBolinha *= -1;
+  }
 }
